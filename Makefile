fe1b0a0d127043f0055dcfa93bec6413e96b9e17
Louis-Philippe Gauthier <lpgauth@gmail.com>
2016-01-26 21:14:31 -0500

    Add profile target

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
+CACHEGRIND=qcachegrind
 REBAR=./rebar3
 
 all: compile
@@ -22,10 +23,20 @@ eunit:
 	@echo "Running rebar3 eunit..."
 	@$(REBAR) do eunit -cv, cover -v
 
+profile:
+	@echo "Profiling..."
+	@$(REBAR) as test compile
+	@erl -noshell \
+	     -pa _build/test/lib/*/ebin \
+		 -eval 'statsderl_profile:fprofx()' \
+		 -eval 'init:stop()'
+	@_build/test/lib/fprofx/erlgrindx -p fprofx.analysis
+	@$(CACHEGRIND) fprofx.cgrind
+
 test: compile dialyzer eunit xref
 
 xref:
 	@echo "Running rebar3 xref..."
 	@$(REBAR) xref
 
-.PHONY: clean compile dialyzer edoc eunit xref
+.PHONY: clean compile dialyzer edoc eunit profile xref
---
+++ test/statsderl_profile.erl
@@ -0,0 +1,50 @@
+-module(statsderl_profile).
+
+-export([
+    fprofx/0
+]).
+
+-define(N, 1000).
+-define(P, 100).
+
+%% public
+-spec fprofx() -> ok.
+
+fprofx() ->
+    Filenames = filelib:wildcard("_build/default/lib/*/ebin/*.beam"),
+    Rootnames = [filename:rootname(Filename, ".beam") || Filename <- Filenames],
+    lists:foreach(fun code:load_abs/1, Rootnames),
+
+    application:start(statsderl),
+    [statsderl:increment(["test", <<".test">>], 1, 0.25) || _ <- lists:seq(1, ?N)],
+
+    fprofx:start(),
+    {ok, Tracer} = fprofx:profile(start),
+    fprofx:trace([start, {procs, new}, {tracer, Tracer}]),
+
+    Self = self(),
+    [spawn(fun () ->
+        [statsderl:increment(["test", <<".test">>], 1, 0.25) || _ <- lists:seq(1, ?N)],
+        Self ! exit
+    end) || _ <- lists:seq(1, ?P)],
+    wait(),
+
+    fprofx:trace(stop),
+    fprofx:analyse([totals, {dest, ""}]),
+    fprofx:stop(),
+
+    application:stop(statsderl),
+
+    ok.
+
+%% private
+wait() ->
+    wait(?P).
+
+wait(0) ->
+    ok;
+wait(X) ->
+    receive
+        exit ->
+            wait(X - 1)
+    end.
