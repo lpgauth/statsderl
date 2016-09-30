b9df0425a3ee794a91b383b150f1d6489cef4e45
Louis-Philippe Gauthier <lpgauth@gmail.com>
2016-09-29 17:53:43 -0700

    Use binaries

--- src/statsderl_protocol.erl
+++ src/statsderl_protocol.erl
@@ -12,21 +12,22 @@
 -spec encode(op_code(), key(), value(), sample_rate()) -> iodata().
 
 encode(counter, Key, Value, SampleRate) ->
-    [Key, ":", format_value(Value), "|c", format_sample_rate(SampleRate)];
+    [Key, <<":">>, format_value(Value), <<"|c">>,
+        format_sample_rate(SampleRate)];
 encode(gauge, Key, Value, _SampleRate) ->
-    [Key, ":", format_value(Value), "|g"];
+    [Key, <<":">>, format_value(Value), <<"|g">>];
 encode(gauge_decrement, Key, Value, _SampleRate) ->
-    [Key, ":-", format_value(Value), "|g"];
+    [Key, <<":-">>, format_value(Value), <<"|g">>];
 encode(gauge_increment, Key, Value, _SampleRate) ->
-    [Key, ":+", format_value(Value), "|g"];
+    [Key, <<":+">>, format_value(Value), <<"|g">>];
 encode(timing, Key, Value, _SampleRate) ->
-    [Key, ":", format_value(Value), "|ms"].
+    [Key, <<":">>, format_value(Value), <<"|ms">>].
 
 %% private
 format_sample_rate(SampleRate) when SampleRate >= 1 ->
-    "";
+    <<>>;
 format_sample_rate(SampleRate) ->
-    ["|@", float_to_list(SampleRate, [{decimals, 3}])].
+    [<<"|@">>, float_to_list(SampleRate, [{decimals, 3}])].
 
 format_value(Value) when is_integer(Value) ->
     integer_to_list(Value);
