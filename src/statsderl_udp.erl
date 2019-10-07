b8239f14049b1c1d92ea24a1ce6fe501909166b7
Louis-Philippe Gauthier <lpgauth@gmail.com>
2019-10-07 08:37:30 -0400

    Add support for OTP-22.1

--- src/statsderl_udp.erl
+++ src/statsderl_udp.erl
@@ -3,6 +3,11 @@
 
 -compile(inline).
 -compile({inline_size, 512}).
+-compile({nowarn_unused_function, [{uint32, 1}]}).
+
+-ignore_xref([
+    {?MODULE, uint32, 1}
+]).
 
 -export([
     header/2
@@ -15,18 +20,29 @@
 -spec header(inet:ip_address(), inet:port_number()) ->
     iodata().
 
--ifdef(UDP_HEADER).
+-ifdef(UDP_HEADER_1).
 
 header(IP, Port) ->
-    [?INET_AF_INET, ?INT16(Port) | ip4_to_bytes(IP)].
+    [?INT16(Port), ip4_to_bytes(IP)].
+
+-else.
+-ifdef(UDP_HEADER_2).
+
+header(IP, Port) ->
+    [?INET_AF_INET, ?INT16(Port), ip4_to_bytes(IP)].
 
 -else.
 
 header(IP, Port) ->
-    [?INT16(Port) | ip4_to_bytes(IP)].
+    [?INET_AF_INET, ?INT16(Port), ip4_to_bytes(IP), uint32(0)].
 
+-endif.
 -endif.
 
 %% private
 ip4_to_bytes({A, B, C, D}) ->
     [A band 16#ff, B band 16#ff, C band 16#ff, D band 16#ff].
+
+uint32(X) ->
+    [((X) bsr 24) band 16#ff, ((X) bsr 16) band 16#ff,
+     ((X) bsr 8) band 16#ff, (X) band 16#ff].
